bed50fd3b0548ee4e67fc2f110a361c354103cac
Moldo <moldovan.alexandru-gheorghe.21@stud.umfst.ro>
2023-01-18 20:55:32 +0200

    V1.1

---
+++ MagazinMOTO/MagazinMOTO/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazaginMOTO
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    class UtilizatorDbContext : DbContext
+    {
+        public DbSet<UserModel> Utilizatori { get; set; }
+    }
+}
